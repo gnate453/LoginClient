8f661aee45e582418127df1c2ed353b450d7f9ef
gnate320 <gnate@rams.colostate.edu>
2015-03-18 18:27:48 -0600

    adding permissions script

---
+++ updatePermissions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+chmod 644 Angularjs/html/*.html
+chmod 644 Angularjs/html/*.css
+chmod 644 Angularjs/scripts/*.js
+chmod 644 Angularjs/textures/*.png
+chmod 644 Angularjs/textures/*.jpg
